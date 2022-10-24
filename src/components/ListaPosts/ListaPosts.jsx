cc39e2345f58398e46cd823578274d29ce96b4ad
Rodrigo Pedroso <99103890+R0drigo-Pedroso@users.noreply.github.com>
2022-10-24 11:50:33 -0300

    Uso do useEffect
    useEffect permitir um maior controle sobre "efeitos colaterais" na execução do componente.

--- src/components/ListaPosts/ListaPosts.jsx
+++ src/components/ListaPosts/ListaPosts.jsx
@@ -11,9 +11,6 @@ const ListaPosts = () => {
   /* Iniciamos o state do componente com um array vazio, para posteriomente "preenchê-lo" com os dados vindos da API. Esta atribuição será feita com auxilio do setPosts (set - servirá para manipulação de dados) */
   const [posts, setPosts] = useState([]);
 
-  console.log(servidorApi);
-  const postsTemp = [];
-
   useEffect(() => {
     async function getPosts() {
       try {
@@ -27,10 +24,23 @@ const ListaPosts = () => {
     getPosts();
   }, []);
 
+  /* Sobre useEffect
+   *Este hook visa permitir um maior controle sobre "efeitos colaterais" na execução do componente.
+
+   Recebe dois parâmetros:
+   1º Função callback com o que será executado (monitora o que foi programado).
+
+   2º Lista de dependencias que indicarão ao useEffect quando ele deverá funcionar.
+
+   -se não passar a lista (ou seja, se deixar sem []), useEffect executará toda vez que o componente for renderizado. Portanto, o callback se torna um loop o infinito.
+
+   -se passar a lista vazia (ou seja, deixar p [] vazio), useEffect executará somente no momento que o componnte é renderizado pela primeira vez, evitando o loop infinito do callback
+   */
+
   return (
     <div className={estilo.lista_posts}>
       {/* Função utilizando map */}
-      {postsTemp.map(({ id, titulo, subtitulo }) => {
+      {posts.map(({ id, titulo, subtitulo }) => {
         return (
           <article key={id} className={estilo.post}>
             <h3>{titulo}</h3>
